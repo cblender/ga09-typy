f1d988cc4985ff041d4512b582add0488b4ac590
cblender <christopher.blender@gmail.com>
2020-09-28 05:12:46 -0400

    peewee error: postgres driver not installed --- continuing work on structure

--- lib/init.sql
+++ lib/init.sql
@@ -1,16 +1,16 @@
 BEGIN;
 
+DROP TABLE notes;
+
 CREATE TABLE notes(
     id integer NOT NULL,
     name text NOT NULL,
     author text NOT NULL,
-    date timestamptz NOT NULL
+    date text NOT NULL
 );
 
-COPY notes (id, name, author, date) FROM stdin;
-1   README  Chris Blendermann   SELECT NOW()
-2   TODO LIST   Somebody    SELECT NOW()
-\.
+INSERT INTO notes (id, name, author, date)
+VALUES(id=1, name='README', author='Chris Blendermann', date='9/28/2020');
 
 ALTER TABLE ONLY notes
     ADD CONSTRAINT notes_pkey PRIMARY KEY (name);
--- lib/typy.py
+++ lib/typy.py
@@ -7,6 +7,27 @@ print('Your IP is {0}'.format(response.json()['origin']))
 from flask import Flask
 from flask import request
 from flask import jsonify
+from peewee import *
+
+# POSTGRESQL SETUP
+db = PostgresqlDatabase('typy', user='postgres', password='', host='localhost', port=5423)
+
+class BaseModel(Model):
+  class Meta:
+    database = db
+
+class Note(BaseModel):
+  id = IntegerField()
+  name = CharField()
+  author = CharField()
+  date = CharField()
+
+db.connect()
+db.drop_tables([Note])
+db.create_tables([Note])
+
+Note(id=1, name='README 2: ELECTRIC BOOGALOO', author='Person McPersonface', date='4/20/1969').save()
+Note(id=2, name='REVENGE OF THE NOTES', author='Literally Jesus', date='12/25/ZERO').save()
 
 app = Flask(__name__)
 
@@ -19,6 +40,8 @@ def index():
 def hello(name):
     return f'Hello, {name}!'
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# THIS CODE IS *** C O N D E M N E D ***
 @app.route('/endpoint', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def endpoint():
     if request.method == 'GET':
@@ -32,22 +55,35 @@ def endpoint():
 
     if request.method == 'DELETE':
         return 'DELETE request'
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 
 # PLANNING!
 # FUNCTIONALITY WILL BE INSTANTIATED VIA ROUTING.
+
 # CREATE ----- @app.route('/new/<name>')
+
+
+
 # READ ------- @app.route('/list/<name>')  ---- NOTE: "READ" MEANS READ OUT THE LIST OF NOTES
+
+
+
 # UPDATE ----- @app.route('/open/<name>') ----- NOTE: "UPDATE" MEANS OPEN A PREEXISTING NOTE, WITH THE ABILITY TO MODIFY IT
+
+
+
 # DESTROY ---- @app.route('/delete/<name>')
-# 
-# 
+
+
 
 
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # NOTHING ELSE GOES BELOW THIS LINE!!!
 
-app.run()
+app.run(debug=True)
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # END
