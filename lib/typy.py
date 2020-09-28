ab74a863f1860e853d70c01deff999973c008da3
cblender <christopher.blender@gmail.com>
2020-09-28 06:36:02 -0400

    the work continues

--- lib/typy.py
+++ lib/typy.py
@@ -12,7 +12,7 @@ import logging
 import sys
 
 # # POSTGRESQL SETUP
-db = PostgresqlDatabase('typy', user='cblender', password='', host='localhost', port=5000)
+db = PostgresqlDatabase('typy', user='cblender', password='', host='localhost', port=5423)
 
 class BaseModel(Model):
   class Meta:
@@ -44,53 +44,76 @@ def hello(name):
 
 
 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# THIS CODE IS *** C O N D E M N E D ***
-# @app.route('/endpoint', methods=['GET', 'PUT', 'POST', 'DELETE'])
-# def endpoint():
-#     if request.method == 'GET':
-#         return 'GET request'
-    
-#     if request.method == 'PUT':
-#         return 'PUT request'
-
-#     if request.method == 'POST':
-#         return 'POST request'
-
-#     if request.method == 'DELETE':
-#         return 'DELETE request'
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
 # PLANNING!
 # FUNCTIONALITY WILL BE INSTANTIATED VIA ROUTING.
 
+
+
+# ——————————————————————————————————————————————————————————————————————————————————————————————————
+#      .aMMMb  dMMMMb  dMMMMMP .aMMMb dMMMMMMP dMMMMMP 
+#     dMP"VMP dMP.dMP dMP     dMP"dMP   dMP   dMP      
+#    dMP     dMMMMK" dMMMP   dMMMMMP   dMP   dMMMP     
+#   dMP.aMP dMP"AMF dMP     dMP dMP   dMP   dMP        
+#   VMMMP" dMP dMP dMMMMMP dMP dMP   dMP   dMMMMMP 
+
 # CREATE ----- @app.route('/new/<name>')
-@app.route('/new/<name>', methods=['GET', 'PUT', 'POST'])
+@app.route('/new/<name>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def note_create(name):
+
+    author = input("Who is the author? ")
+    date = input("What is the date? ")
+    
+    # NOTE TO SELF: REPLACE WITH PROPER ID - EITHER RANDOM OR SEQUENTIAL
+    id = 1
+
+    Note.create(id=f'{id}', name=f'{name}', author=f'{author}', date=f'{date}')
+
     return f'CREATE NOTE NAMED {name}'
 
 
 
-# READ ------- @app.route('/list/<name>')  ---- NOTE: "READ" MEANS READ OUT THE LIST OF NOTES
-@app.route('/list/', methods=['GET'])
-def note_list():
-    return f'LIST ALL NOTES'
-
-
+# ——————————————————————————————————————————————————————————————————————————————————————————————————
+#       .aMMMb  dMMMMb  dMMMMMP dMMMMb 
+#      dMP"dMP dMP.dMP dMP     dMP dMP 
+#     dMP dMP dMMMMP" dMMMP   dMP dMP  
+#    dMP.aMP dMP     dMP     dMP dMP   
+#    VMMMP" dMP     dMMMMMP dMP dMP 
 
 # UPDATE ----- @app.route('/open/<name>') ----- NOTE: "UPDATE" MEANS OPEN A PREEXISTING NOTE, WITH THE ABILITY TO MODIFY IT
-@app.route('/open/<name>', methods=['GET', 'PUT', 'POST'])
+@app.route('/open/<name>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def note_open(name):
+
     return f'OPEN {name} FOR EDITING'
 
 
+
+# ——————————————————————————————————————————————————————————————————————————————————————————————————
+#    dMMMMMMP dMMMMb  .aMMMb  .dMMMb  dMP dMP 
+#      dMP   dMP.dMP dMP"dMP dMP" VP dMP dMP  
+#     dMP   dMMMMK" dMMMMMP  VMMMb  dMMMMMP   
+#    dMP   dMP"AMF dMP dMP dP .dMP dMP dMP    
+#   dMP   dMP dMP dMP dMP  VMMMP" dMP dMP  
+
 # DESTROY ---- @app.route('/delete/<name>')
-@app.route('/delete/<name>', methods=['GET', 'POST', 'DELETE'])
+@app.route('/delete/<name>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def note_delete(name):
+
     return f'DELETE {name} - ARE YOU SURE?'
 
+
+
+# ——————————————————————————————————————————————————————————————————————————————————————————————————
+#       dMP     dMP .dMMMb dMMMMMMP 
+#      dMP     amr dMP" VP   dMP    
+#     dMP     dMP  VMMMb    dMP     
+#    dMP     dMP dP .dMP   dMP      
+#   dMMMMMP dMP  VMMMP"   dMP 
+
+# READ ------- @app.route('/list/<name>')  ---- NOTE: "READ" MEANS READ OUT THE LIST OF NOTES
+@app.route('/list/', methods=['GET', 'PUT', 'POST', 'DELETE'])
+def note_list():
+
+    return f'LIST ALL NOTES'
 
 
 
